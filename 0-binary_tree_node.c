f9552a65a74976137a89b0c79e106885679a78a5
mickay <michealaderibigbe@gmail.com>
2024-05-29 11:06:42 +0100

    binary

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,2 +1,14 @@
-include <stdlib.h>
-include "binary_trees.h"
+#include <stdio.h>
+int main(){
+   int LA[3] = {}, i;
+   printf("Array Before Insertion:\n");
+   for(i = 0; i < 3; i++)
+      printf("LA[%d] = %d \n", i, LA[i]);
+   printf("Inserting Elements.. \n");
+   printf("The array elements after insertion :\n"); // prints array values
+   for(i = 0; i < 3; i++) {
+      LA[i] = i + 2;
+      printf("LA[%d] = %d \n", i, LA[i]);
+   }
+   return 0;
+}
